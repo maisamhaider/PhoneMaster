09b02556f5f870bac1c07269b46f24f0e9805a3e
umar-786 <uak4589@gmail.com>
2020-06-12 10:44:45 +0500

    12/jun/20 1

---
+++ app/src/main/java/com/example/phonemaster/receivers/BootUpReceiver.java
@@ -0,0 +1,29 @@
+package com.example.phonemaster.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import com.example.phonemaster.services.MyService;
+
+public class BootUpReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        /****** For Start Activity *****/
+//        Intent i = new Intent(context, MyActivity.class);
+//        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        context.startActivity(i);
+
+
+        // TODO add prefence
+        /***** For start Service  ****/
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            context.startForegroundService(new Intent(context, MyService.class));
+        }else{
+            context.startService(new Intent(context, MyService.class));
+        }
+    }   
+
+}
