121c9fc81cfa4509b3670d98efab5f33bc687fb4
maisamhaider <maisamhaiderturi@gmail.com>
2020-06-13 10:12:41 +0500

    13/jun/20 1

---
+++ app/src/main/java/com/example/phonemaster/activities/JunkFilesAct.java
@@ -0,0 +1,16 @@
+package com.example.phonemaster.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.phonemaster.R;
+
+public class JunkFilesAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_junk_files_atc);
+    }
+}
