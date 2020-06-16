c1c3363764802bd1f2bb018fcd97533e035d9204
umar-786 <uak4589@gmail.com>
2020-06-16 18:27:00 +0500

    umar 15/june/2020 off time

--- app/src/main/java/com/example/phonemaster/fragments/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/phonemaster/fragments/dashboard/DashboardFragment.java
@@ -2,45 +2,35 @@ package com.example.phonemaster.fragments.dashboard;
 
 import android.app.ActivityManager;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.app.adprogressbarlib.AdCircleProgress;
 import com.example.phonemaster.R;
-import com.example.phonemaster.activities.BatterySavingAct;
-import com.example.phonemaster.activities.CleanWhatsAppAct;
-import com.example.phonemaster.activities.CpuCooler;
-import com.example.phonemaster.activities.DeepCleanAct;
-import com.example.phonemaster.activities.HarassmentFilterAct;
-import com.example.phonemaster.activities.PhoneBoostAct;
-import com.example.phonemaster.activities.SmartChargingAct;
-import com.example.phonemaster.activities.UnInstallAppAct;
-import com.example.phonemaster.activities.WhatsAppStatusAct;
 import com.example.phonemaster.permission.Permissions;
 import com.example.phonemaster.utils.Utils;
 
-import java.util.Objects;
-
 
 public class DashboardFragment extends Fragment implements View.OnClickListener {
+
     private static final int REQUEST_PERMISSION = 1000;
     private Permissions permissions;
     private Utils utils;
     private SharedPreferences preferences;
     private int REQUEST_READ_PHONE_STATE = 5005;
     private View root;
+    private AdCircleProgress cpbRam, cpbStorage;
+    private ProgressBar pbRam, pbStorage;
+    private TextView tvRamInfo, tvStorageInfo;
 
     private DashboardViewModel dashboardViewModel;
 
@@ -55,11 +45,20 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
         return root;
     }
 
-    public void init(){
+    public void init() {
         permissions = new Permissions(getActivity());
         permissions.permission();
         utils = new Utils(getActivity());
-        preferences = getActivity().getSharedPreferences("myPref",Context.MODE_PRIVATE);
+        preferences = getActivity().getSharedPreferences("myPref", Context.MODE_PRIVATE);
+
+        cpbRam = root.findViewById(R.id.pgb_ram);
+        cpbStorage = root.findViewById(R.id.pgb_storage);
+
+        pbRam = root.findViewById(R.id.pb_ram);
+        pbStorage = root.findViewById(R.id.pb_storage);
+        tvRamInfo = root.findViewById(R.id.tv_ram_info);
+        tvStorageInfo = root.findViewById(R.id.tv_storage_info);
+
 
 //        Button cleanWhatsApp_btn = root.findViewById(R.id.cleanWhatsApp_btn);
 //        Button cpuCooler_btn = root.findViewById(R.id.cpuCooler_btn);
@@ -90,18 +89,22 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
     }
 
 
-    private void ramAndStorageFun()
-    {
-        TextView ramPercent_tv = root.findViewById(R.id.ramPercent_tv);
-        TextView storageUsagePercent_tv = root.findViewById(R.id.storageUsagePercent_tv);
+    private void ramAndStorageFun() {
+//        TextView ramPercent_tv = root.findViewById(R.id.ramPercent_tv);
+//        TextView storageUsagePercent_tv = root.findViewById(R.id.storageUsagePercent_tv);
 //for Ram
-        ActivityManager activityManager = (ActivityManager) getActivity().getSystemService( Context.ACTIVITY_SERVICE );
+        ActivityManager activityManager = (ActivityManager) getActivity().getSystemService(Context.ACTIVITY_SERVICE);
         ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
-        activityManager.getMemoryInfo( memoryInfo );
+        activityManager.getMemoryInfo(memoryInfo);
         float totalRam1 = memoryInfo.totalMem / 1024 / 1024;
-        float freeRam1 = memoryInfo.availMem / 1024/ 1024;
-        float usedRam =  totalRam1 - freeRam1;
-        ramPercent_tv.setText( String.format( "%.1f",utils.getPercentage( totalRam1,usedRam ) )+"%" );
+        float freeRam1 = memoryInfo.availMem / 1024 / 1024;
+        float usedRam = totalRam1 - freeRam1;
+//        ramPercent_tv.setText( String.format( "%.1f",utils.getPercentage( totalRam1,usedRam ) )+"%" );
+        cpbRam.setAdProgress((int) utils.getPercentage( totalRam1,usedRam ));
+        pbRam.setProgress((int) utils.getPercentage( totalRam1,usedRam ));
+        tvRamInfo.setText(String.format("%.0fMB/%.0fMB",usedRam,totalRam1));
+
+
         //for Storage
         float totalStorageInGBs, availableStorageInGBs, usedStorageInGBs;
 
@@ -109,7 +112,13 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
         availableStorageInGBs = utils.getAvailableStorage() / 1024;
 
         usedStorageInGBs = totalStorageInGBs - availableStorageInGBs;
-        storageUsagePercent_tv.setText(String.format( "%.1f", utils.getPercentage( totalStorageInGBs, usedStorageInGBs )  )+ "%" );
+
+        cpbStorage.setAdProgress((int) utils.getPercentage( totalStorageInGBs, usedStorageInGBs ));
+        pbStorage.setProgress((int) utils.getPercentage( totalStorageInGBs, usedStorageInGBs ));
+        tvStorageInfo.setText(String.format("%.0fGB/%.0fGB",usedStorageInGBs,totalStorageInGBs));
+
+
+//        storageUsagePercent_tv.setText(String.format( "%.1f", utils.getPercentage( totalStorageInGBs, usedStorageInGBs )  )+ "%" );
 
     }
 
