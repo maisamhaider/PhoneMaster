4913d711e7fb0c9412f2d0d7936ad08a5aecb24a
umar-786 <uak4589@gmail.com>
2020-06-17 12:53:26 +0500

    umar 17/june/2020 first

--- app/src/main/java/com/example/phonemaster/fragments/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/phonemaster/fragments/dashboard/DashboardFragment.java
@@ -2,11 +2,13 @@ package com.example.phonemaster.fragments.dashboard;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -16,6 +18,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.app.adprogressbarlib.AdCircleProgress;
 import com.example.phonemaster.R;
+import com.example.phonemaster.activities.UnInstallAppAct;
 import com.example.phonemaster.permission.Permissions;
 import com.example.phonemaster.utils.Utils;
 
@@ -66,7 +69,7 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
 //        Button batterySaving_btn = root.findViewById(R.id.batterySaving_btn);
 //        Button whatsAppStatusSaver_btn = root.findViewById(R.id.whatsAppStatusSaver_btn);
 //        Button harassmentFilter_btn = root.findViewById(R.id.harassmentFilter_btn);
-//        Button unInstallApp_btn = root.findViewById(R.id.unInstallApp_btn);
+        ImageView unInstallApp_iv = root.findViewById(R.id.ic_app_cleanup);
 //        Button deepClean_btn = root.findViewById(R.id.deepClean_btn);
 //        Button smartCharging_btn = root.findViewById(R.id.smartCharging_btn);
 //        Button junkClean_btn = root.findViewById(R.id.junkClean_btn);
@@ -77,7 +80,7 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
 //        batterySaving_btn.setOnClickListener(this);
 //        whatsAppStatusSaver_btn.setOnClickListener(this);
 //        harassmentFilter_btn.setOnClickListener(this);
-//        unInstallApp_btn.setOnClickListener(this);
+        unInstallApp_iv.setOnClickListener(this);
 //        deepClean_btn.setOnClickListener(this);
 //        smartCharging_btn.setOnClickListener(this);
 //        junkClean_btn.setOnClickListener(this);
@@ -124,8 +127,8 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
 
     @Override
     public void onClick(View v) {
-//        switch (v.getId())
-//        {
+        switch (v.getId())
+        {
 //            case R.id.cleanWhatsApp_btn:
 //                if (permissions.permission())
 //                {
@@ -165,10 +168,10 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
 //                Intent harassmentFilterIntent = new Intent(getActivity(), HarassmentFilterAct.class);
 //                startActivity(harassmentFilterIntent);
 //                break;
-//            case R.id.unInstallApp_btn:
-//                Intent unInstallAppIntent = new Intent(getActivity(), UnInstallAppAct.class);
-//                startActivity(unInstallAppIntent);
-//                break;
+            case R.id.ic_app_cleanup:
+                Intent unInstallAppIntent = new Intent(getActivity(), UnInstallAppAct.class);
+                startActivity(unInstallAppIntent);
+                break;
 //            case R.id.deepClean_btn:
 //                Intent deepCleanIntent = new Intent(getActivity(), DeepCleanAct.class);
 //                startActivity(deepCleanIntent);
@@ -178,7 +181,7 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
 //                startActivity(smartChargingIntent);
 //                break;
 //
-//        }
+        }
     }
 
 }
